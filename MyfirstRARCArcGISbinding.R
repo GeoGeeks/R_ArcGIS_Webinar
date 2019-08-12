678132f1938d060b952f91bebbf052662a78dd12
hedtorresca <hedtorresca@unal.edu.co>
2019-08-12 11:27:17 +0000

    third commit

--- MyfirstRARCArcGISbinding.R
+++ MyfirstRARCArcGISbinding.R
@@ -2,8 +2,6 @@
 # Carga de paquetes necesarios
 
 library (reshape2)
-library (ggplot2)
-library (ggmap)
 library(sp)
 library(spdep)
 library(dplyr)
@@ -17,7 +15,7 @@ arc.check_product()
 
 
 # Carga de conjuntos de datos, tablas y capas de ArcGIS en un espacio de trabajo de R.
-enrichdf <- arc.open(path="C:\\R para desarrolladores\\seguridad.gdb\\hotspothomicidiosenriquecido")
+enrichdf <- arc.open(path="seguridad.gdb\\hotspothomicidiosenriquecido")
 enrichdf
 
 columnasDS <- c( 'OBJECTID','SUM_VALUE','TOTPOP','TOTMALES','TOTFEMALES','PAGE02_CY', 'PAGE03_CY', 'PAGE04_CY', 'PAGE05_CY','CS01_CY', 'CS02_CY','CS03_CY', 'CSPC10_CY', 'CS13_CY', 'PP_CY', 'PPPC_CY')
@@ -29,17 +27,13 @@ enrich_select_df
 # Conversión de un objeto de marco arc.data a un objeto de marco de datos sp.
 enrich_spdf <- arc.data2sp(enrich_select_df)
 enrich_spdf
-
-# Cambio de nombres de variables seleccionadas
-columnasDS <- c( 'OBJECTID','SUM_VALUE','TOTPOP','TOTMALES','TOTFEMALES','PAGE02_CY', 'PAGE03_CY', 'PAGE04_CY', 'PAGE05_CY','CS01_CY', 'CS02_CY','CS03_CY', 'CSPC10_CY', 'CS13_CY', 'PP_CY', 'PPPC_CY')
-colnames(enrich_spdf@data) <- columnasDS
 colnames(enrich_spdf@data)
-
+enrich_spdf@data <- enrich_spdf@data[enrich_spdf@data$TOTPOP>0,]
 
 # Calcular las tasas de criminalidad suavizadas de Bayes empíricas para cada hexagono.
 
-n <- filtrado$SUM_VALUE
-x <- filtrado$TOTPOP
+n <- enrich_spdf@data$SUM_VALUE
+x <- enrich_spdf@data$TOTPOP
 
 # La función EBest () R realiza un tipo particular de estimación bayesiana empírica.
 EB <- EBest (n, x)
@@ -51,8 +45,6 @@ a <- attr(EB, "parameters")$a
 v <- a + (b/x)
 v[v < 0] <- b/x
 z <- (p - b)/sqrt(v)
-m <- attr(EB, "parameters")$m
-m
 a 
 b
 EB
@@ -64,4 +56,4 @@ enrich_spdf@data$EB_Rate <- z
 
 # Exportar un objeto de marco de datos a un conjunto de datos de ArcGIS.
 
-arc.write("C:\\R para desarrolladores\\seguridad.gdb\\hotspothomicidiosenriquecidosuavizado2",enrich_spdf)
+arc.write("seguridad.gdb\\hotspothomicidiosenriquecidosuavizado2",enrich_spdf)
